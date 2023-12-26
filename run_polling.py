88d14a5d84c3f694c1621a9eb73b5f1fca12459e
seocringe <privetnambogolet@gmail.com>
2023-12-26 16:59:15 +0200

    modified:   run_polling.py

--- run_polling.py
+++ run_polling.py
@@ -1,35 +1,32 @@
-"""
-This module provides a function to run a bot in polling mode.
-"""
+import os, django
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dtb.settings')
+django.setup()
+
+from telegram import Bot
 from telegram.ext import Updater
+
 from dtb.settings import TELEGRAM_TOKEN
 from tgbot.dispatcher import setup_dispatcher
 
 
-class SpecificException(Exception):
-    """
-    This is a specific exception class.
-    """
+def run_polling(tg_token: str = TELEGRAM_TOKEN):
+    """ Run bot in polling mode """
+    updater = Updater(tg_token, use_context=True)
 
+    dp = updater.dispatcher
+    dp = setup_dispatcher(dp)
 
-def run_polling(tg_token=None):
-    """Run bot in polling mode."""
-    if tg_token is None:
-        tg_token = TELEGRAM_TOKEN
+    bot_info = Bot(tg_token).get_me()
+    bot_link = f"https://t.me/{bot_info['username']}"
 
-    try:
-        updater = Updater(token=str(tg_token), use_context=True)
-        setup_dispatcher(updater.dispatcher)
-        print(
-            "Polling of 'https://t.me/"
-            + updater.bot.get_me()["username"].strip()
-            + "' has started"
-        )
-        updater.start_polling()
-        updater.idle()
-    except SpecificException as error:
-        print(f"A specific error occurred: {error}")
+    print(f"Polling of '{bot_link}' has started")
+    # it is really useful to send '👋' emoji to developer
+    # when you run local test
+    # bot.send_message(text='👋', chat_id=<YOUR TELEGRAM ID>)
+
+    updater.start_polling()
+    updater.idle()
 
 
 if __name__ == "__main__":

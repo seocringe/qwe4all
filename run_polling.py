35b6a43d62406fd0c83fdecf5216e0391dc705ba
seocringe <privetnambogolet@gmail.com>
2023-12-26 16:12:54 +0200

    Refactor function to improve performance

--- run_polling.py
+++ run_polling.py
@@ -1,62 +1,33 @@
-"""
-Этот модуль запускает бота в режиме опроса.
-"""
-
-import os
-
-# Импортируем модуль django для настройки Django-проекта
-import django
-from telegram import Bot  # Импортируем класс Bot для работы с Telegram API
-
-# Импортируем Updater для обработки входящих обновлений
 from telegram.ext import Updater
-
-# Импортируем токен Telegram из настроек проекта
 from dtb.settings import TELEGRAM_TOKEN
-
-# Импортируем функцию для настройки диспетчера
 from tgbot.dispatcher import setup_dispatcher
 
-# Задаем переменную окружения, указывающую на файл настроек Django
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dtb.settings")
-django.setup()  # Настраиваем Django
 
-
-def run_polling(tg_token: str = TELEGRAM_TOKEN):
-    """Запустить бота в режиме опроса.
-
-    Аргументы:
-        tg_token (str): Токен Telegram, используемый для проверки подлинности бота.
-
-    Возвращает:
-        None
+class SpecificException(Exception):
     """
-    from telegram.ext import Dispatcher  # Import the Dispatcher class
-
-    from telegram.ext import Dispatcher  # Import the Dispatcher class
-
-    updater = Updater(
-        tg_token, use_context=True
-    )  # Create an instance of Updater to receive updates
-
-    # Specify the type of "dispatcher" as "Dispatcher"
-    dp: Dispatcher = updater.dispatcher
-    # Configure the dispatcher using the custom function
-    dp = setup_dispatcher(dp)
-
-    # Get bot information and create a link to it
-    bot_info = Bot(tg_token).get_me()
-    bot_link = f"https://t.me/{bot_info['username']}"
-
-    # Выводим сообщение о старте опроса
-    print(f"Получение обновлений для '{bot_link}' началось")
-    # Это может быть полезно для отправки эмодзи разработчику при локальном тестировании
-    # bot.send_message(text='👋', chat_id=<ВАШ TELEGRAM ID>)
-
-    updater.start_polling()  # Начинаем опрос серверов Telegram
-    updater.idle()  # Запускаем бесконечный цикл приема обновлений
+    This is a specific exception class.
+    """
 
 
-# Этот блок выполнится, если скрипт запущен как главный модуль
+def run_polling(tg_token=None):
+    """Run bot in polling mode."""
+    # Use the provided token or fallback to the one from settings
+    if tg_token is None:
+        tg_token = TELEGRAM_TOKEN
+
+    try:
+        updater = Updater(token=str(tg_token), use_context=True)
+        setup_dispatcher(updater.dispatcher)
+        print(
+            "Polling of 'https://t.me/" 
+            + updater.bot.get_me()['username'] 
+            + "' has started"
+        )
+        updater.start_polling()
+        updater.idle()
+    except SpecificException as error:
+        print(f"A specific error occurred: {error}")
+
+
 if __name__ == "__main__":
     run_polling()
